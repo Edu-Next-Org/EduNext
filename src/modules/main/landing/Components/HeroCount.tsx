31184663a6635ee61da686186fbab785bfac72e1
Abolfazl <abolfazlmnf83@gmail.com>
2026-06-27 17:01:34 +0330

    fixed countup

--- src/modules/main/landing/Components/HeroCount.tsx
+++ src/modules/main/landing/Components/HeroCount.tsx
@@ -6,7 +6,7 @@ function HeroCount() {
     <div className="flex lg:flex-row flex-col py-2  lg:px-10 justify-center lg:justify-between items-center dark:bg-[#333] rounded-2xl">
       <div className="flex gap-3 items-center  ">
         <h1 className=" text-[24px] lg:text-[32px] font-bold text-violet-600 ">
-          <CountUp end={12} duration={4} />
+          <CountUp end={12} duration={11} />
           k+
         </h1>
 
@@ -14,13 +14,13 @@ function HeroCount() {
       </div>
       <div className="flex gap-3 items-center border-y-2 lg:border-y-0 lg:border-x-2 border-[#dddd] py-2 lg:py-0 lg:px-10 ">
         <h1 className="text-[24px] lg:text-[32px] font-bold text-violet-600 ">
-          <CountUp end={350} duration={4} />+
+          <CountUp end={350} duration={11} />+
         </h1>
         <h3 className="lg:text-[22px]">Expert Instructors</h3>
       </div>
       <div className="flex gap-3 items-center ">
         <h1 className="text-[24px] lg:text-[32px] font-bold text-violet-600 ">
-          <CountUp end={54} duration={4} />+
+          <CountUp end={54} duration={11} />+
         </h1>
         <h3 className="lg:text-[22px]">Lessons Viewed </h3>
       </div>
